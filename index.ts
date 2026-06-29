15acd4c909c843a71d6105a5a2ce0d3f9a07f8e2
Gustavo Caravajal <Gustavocarvajal_2001@outlook.es>
2026-06-28 22:54:22 -0300

    Crear servidor express

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Bienvenidos a nuestra API REST");
+});
+
+const PORT: number = 3000;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
